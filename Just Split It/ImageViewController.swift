5261e2f284fd97bed17fa37d526203d37ecdd381
Muhammad Joyo <muhammadjoyo@nyu.edu>
2017-12-20 02:40:55 -0500

    Implemented quantity in parsing Only allows for up to 10 items

--- Just Split It/ImageViewController.swift
+++ Just Split It/ImageViewController.swift
@@ -47,6 +47,7 @@ class ImageViewController: UIViewController {
 		//NSRegexExpressions
 		let taxRegex = try! NSRegularExpression(pattern: "(T|t)(a|A|c|C|o|O|\\s)(X|x)", options: [])
 		let priceRegex = try! NSRegularExpression(pattern: "[0-9]*\\s?[\\.|,]\\s?[0-9][0-9]", options: [])
+		let quantityRegex = try! NSRegularExpression(pattern: "(^[0-9]+)(\\s)*", options: [])
 		
 		//When true, stops adding items
 		var reachedEndOfItems = false
@@ -57,6 +58,8 @@ class ImageViewController: UIViewController {
 			let priceMatches = priceRegex.matches(in: current, range: NSRange(location: 0, length: current.count))
 			//Looks for the first match of the word "tax"
 			let taxMatch = taxRegex.firstMatch(in: current, range: NSRange(location: 0, length: current.count))
+			//Looks for the first match of a number at the beginning of the string
+			let quantityMatch = quantityRegex.firstMatch(in: current, range: NSRange(location: 0, length: current.count))
 			//Selects the last match of price in the line
 			let priceMatch: NSTextCheckingResult?
 			if priceMatches.count > 0 {
@@ -82,13 +85,35 @@ class ImageViewController: UIViewController {
 					reachedEndOfItems = true
 					continue
 				}
+				//Default quantity is 1
+				var quantity = 1
+				let itemName: String?
+				if(quantityMatch != nil) {
+					//Creates a substring of the quantity without the space after
+					let itemQuantity = currNS?.substring(with: quantityMatch!.range(at: 0)) as NSString?
+					//Only sets it as the quantity if it's less than 10
+					if(itemQuantity!.intValue <= 10) {
+						quantity = Int(itemQuantity!.intValue)
+					}
+					//Creates a substring from the end of quantity to the beginning of price
+					itemName = currNS?.substring(with: NSMakeRange(quantityMatch!.range.length, priceMatch!.range.location - quantityMatch!.range.length))
+				} else {
+					//Creates a substring of the rest of the string to get the name
+					itemName = currNS?.substring(with: NSMakeRange(0, priceMatch!.range.location))
+				}
 				//Takes the range from priceMatch and get's the subtring of the match
 				let price = currNS?.substring(with: priceMatch!.range) as NSString?
-				//Creates a substring of the string that isn't the price to get the name
-				let nameRange = NSMakeRange(0, priceMatch!.range.location)
-				//Adds new item to the group bill
-				let newItem = Item(name: (currNS?.substring(with: nameRange))!, price: price?.floatValue ?? 0)
-				newGroupBill.addItem(item: newItem)
+				//Creates a character set of character not wanted in the name
+				let charactersToRemove = NSCharacterSet(charactersIn: "ABCDEFGHIJKLMNOPQRSTUVW‌​XYZabcdefghijklmnopq‌​rstuvwxyz0123456789.‌​_- ").inverted
+				//Removes all unwanted characters from the character set
+				let cleanName = itemName?.components(separatedBy: charactersToRemove).joined(separator: "")
+				//Calculate actual price
+				let itemPrice = (price?.floatValue)!/Float(quantity)
+				//Adds new items to the group bill based on the quantity
+				for _ in 1...quantity {
+					let newItem = Item(name: cleanName!, price: itemPrice)
+					newGroupBill.addItem(item: newItem)
+				}
 			}
 		}
 	}
